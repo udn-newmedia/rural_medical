d3124cad400d36b263ac430e426eb625a6180a81
udn-newmedia@gmail.com <udn.newmedia@gamil.com>
2018-01-03 16:47:46 +0800

    change button and logo

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -21,15 +21,16 @@ export default {
     .question-button{
         width: 100%;
         max-width: 500px;
-        height: 80px;
-        border-radius: 40px;
-        background-color: #ef4326;
+        height: 70px;
+        border-radius: 35px;
+        border: solid 2px #DCDCDC;
+        background-color: #FFFFFF;
         box-shadow: -2.8px 1.1px 7px 0 rgba(0, 0, 0, 0.1);
         text-align: center;
-        color: #FFFFFF;
+        color: #636363;
         letter-spacing: -0.4px;
         font-size: 18px;
-        line-height: 80px;
+        line-height: 70px;
     }
 
     @media screen and (min-width: 1024px){
